72330c62f553b94897690c54e90ca64bc9cd3a88
Diego Alzate <diego35.da@gmail.com>
2022-04-06 21:31:19 -0500

    start monorepo design added nohoist object

--- packages/hardhat/contracts/RadNFT.sol
+++ packages/hardhat/contracts/RadNFT.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
+
 import { Base64 } from "./libraries/Base64.sol";
 contract RadNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
